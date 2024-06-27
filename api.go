1ac1fc4868c07fdc825c68d978d11ca0bf4a7992
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-27 09:53:50 +0545

    types defined for api routes

--- api.go
+++ api.go
@@ -64,7 +64,9 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	account := NewAccount("Sajin", "Shrestha")
+
+	return WriteJSON(w, http.StatusOK, account)
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
---
+++ types.go
@@ -0,0 +1,20 @@
+package main
+
+import "math/rand"
+
+type Account struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Number    int64
+	Balance   int64
+}
+
+func NewAccount(firstName, lastName string) *Account {
+	return &Account{
+		ID:        rand.Intn(10000),
+		FirstName: firstName,
+		LastName:  lastName,
+		Number:    int64(rand.Intn(1000000)),
+	}
+}
